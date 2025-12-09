86d263269e191d75d91e3a379b17f696c06428f7
David Hovey <davidhovey18@gmail.com>
2025-12-09 13:48:01 -0500

    Wrote b attack

--- createdataB.c
+++ createdataB.c
@@ -4,9 +4,25 @@ padding to overrun the stack, and the address of the instruction
 in main to get a B, the latter of which will overwrite getName's
 stored x30
 */
+#include <stdio.h>
+#include <string.h>
 
 
 /* FUNCTION COMMENT */
 int main(int argc) {
-    printf("Hello World\n");
+    unsigned int uiData;
+    char name[] = "David";
+
+    file *psFile = fopen("davidBgrade.txt", "w");
+
+    if (!psFile) {
+        perror("fopen did not return a valid pointer");
+        return 1; 
+    }
+
+    fwrite(&name, strlen(name), 1, psFile); /* Check case that this does not return */
+
+    fclose(psFile);
+
+    return 0;
 }
